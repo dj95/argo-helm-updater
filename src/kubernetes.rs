f2c72b9d1a49ab860fbda83af0da7fe1daa8abb9
Daniel Jankowski <daniel.jankowski@rub.de>
2023-06-11 12:00:00 +0200

    feat: implement update functionality

--- src/kubernetes.rs
+++ src/kubernetes.rs
@@ -1,8 +1,17 @@
 use anyhow::Ok;
-use kube::{api::ListParams, client::ConfigExt, config::KubeConfigOptions, Api, Client, Config};
+use kube::{
+    api::{ListParams, Patch, PatchParams},
+    client::ConfigExt,
+    config::KubeConfigOptions,
+    Api, Client, Config, ResourceExt,
+};
 use kube_derive::CustomResource;
+use log::debug;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+
+use crate::helm::HelmChart;
 
 pub async fn init_client(
     context: Option<String>,
@@ -80,10 +89,17 @@ impl Application {
 #[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
 pub struct SourceSpec {
     pub chart: Option<String>,
-    #[serde(alias = "repoURL")]
+    #[serde(rename = "repoURL")]
     pub repo_url: Option<String>,
-    #[serde(alias = "targetRevision")]
+    #[serde(rename = "targetRevision")]
     pub target_revision: Option<String>,
+    pub helm: Option<Value>,
+    #[serde(rename = "ref")]
+    pub reference: Option<Value>,
+    pub path: Option<String>,
+    pub kustomize: Option<Value>,
+    pub directory: Option<Value>,
+    pub plugin: Option<Value>,
 }
 
 impl SourceSpec {
@@ -103,4 +119,60 @@ pub async fn list_applications(client: &Client) -> anyhow::Result<Vec<Applicatio
     }
 
     return Ok(output);
+}
+
+pub async fn patch_application(
+    client: &Client,
+    argo_application: &Application,
+    helm: &HelmChart,
+    new_revision: &str,
+) -> Result<Application, kube::Error> {
+    let apps_api: Api<Application> = Api::default_namespaced(client.clone());
+
+    let patch = get_application_patch(argo_application, helm, new_revision);
+
+    debug!("{}", patch);
+
+    return apps_api
+        .patch(
+            &argo_application.name_any(),
+            &PatchParams::apply("argo-helm-updater"),
+            &Patch::Merge(patch),
+        )
+        .await;
+}
+
+fn get_application_patch(
+    argo_application: &Application,
+    helm: &HelmChart,
+    new_revision: &str,
+) -> Value {
+    if argo_application.helm_in_sources() {
+        let sources = argo_application.spec.sources.clone().unwrap();
+        let mut patched_sources: Vec<SourceSpec> = Vec::new();
+
+        for source in sources {
+            let mut patched_source = source.clone();
+
+            if source.chart.is_some() && helm.chart.cmp(&(source.clone()).chart.unwrap()).is_eq() {
+                patched_source.target_revision = Some(new_revision.clone().to_string());
+            }
+
+            patched_sources.push(patched_source);
+        }
+
+        return json!({ "spec": { "sources": patched_sources } });
+    }
+
+    let mut patched_source = argo_application.spec.source.clone().unwrap();
+
+    if helm
+        .chart
+        .cmp(&(patched_source.clone()).chart.unwrap())
+        .is_eq()
+    {
+        patched_source.target_revision = Some(new_revision.to_string());
+    }
+
+    return json!({ "spec": { "source": patched_source } });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use anyhow::bail;
 use clap::Parser;
-use kube::ResourceExt;
-use kubernetes::{init_client, Application, SourceSpec};
+use inquire::Confirm;
+use kube::{Client, ResourceExt};
+use kubernetes::{init_client, patch_application, Application, SourceSpec};
+use log::info;
 
 use crate::{helm::HelmChart, kubernetes::list_applications};
 
@@ -15,11 +18,16 @@ struct Args {
 
     #[arg(short, long)]
     context: Option<String>,
+
+    #[arg(long, default_value_t = false)]
+    update: bool,
 }
 
 pub async fn verify_helm_source(
+    client: &Client,
     argo_application: &Application,
     source_spec: &SourceSpec,
+    should_ask_for_update: bool,
 ) -> anyhow::Result<()> {
     let helm = HelmChart::try_from(source_spec.clone());
 
@@ -31,22 +39,63 @@ pub async fn verify_helm_source(
     let newest_version = helm.get_newer_version().await?;
 
     if let Some(newest_version) = newest_version {
-        println!(
+        info!(
             "{} has new version {} instead of {}",
             argo_application.name_any(),
             newest_version,
             helm.revision
         );
 
-        // TODO: patch kubernetes manifest for new version
+        if should_ask_for_update {
+            ask_for_update(client, argo_application, &helm, &newest_version).await?;
+        }
     }
 
     return Ok(());
 }
 
+async fn ask_for_update(
+    client: &Client,
+    argo_application: &Application,
+    helm: &HelmChart,
+    newest_version: &str,
+) -> anyhow::Result<()> {
+    let ans = Confirm::new(
+        &format!(
+            "Do you want to update {} from {} to {}?",
+            argo_application.name_any(),
+            helm.revision,
+            newest_version,
+        )
+        .to_string(),
+    )
+    .with_default(false)
+    .with_help_message("Don't forget to also update the argo files in your git repo!")
+    .prompt();
+
+    match ans {
+        Ok(true) => {
+            patch_application(client, argo_application, &helm, newest_version).await?;
+
+            info!("successfully update the application spec");
+
+            return Ok(());
+        }
+        Ok(false) => {
+            info!("not updating the chart");
+
+            return Ok(());
+        }
+        Err(_) => bail!("cannot get user confirmation to update"),
+    }
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+    env_logger::init_from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    );
 
     let client = init_client(args.context, args.namespace).await?;
 
@@ -57,12 +106,12 @@ async fn main() -> anyhow::Result<()> {
         }
 
         if a.helm_in_source() {
-            verify_helm_source(&a, &(a.clone()).spec.source.unwrap()).await?;
+            verify_helm_source(&client, &a, &(a.clone()).spec.source.unwrap(), args.update).await?;
         }
 
         if a.helm_in_sources() {
             for source in (a.clone()).spec.sources.unwrap() {
-                verify_helm_source(&a, &source).await?;
+                verify_helm_source(&client, &a, &source, args.update).await?;
             }
         }
     }
