d40de6c9b8efd3308d04e37d75668507b7f04d9d
Daniel Jankowski <daniel.jankowski@rub.de>
2024-05-26 12:02:20 +0200

    chore: move to nix flake instead of shell

--- flake.nix
+++ flake.nix
@@ -2,20 +2,70 @@
   description = "Helps you to identify outdated helm charts in your argocd instance.";
 
   inputs = {
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.follows = "rust-overlay/flake-utils";
-    nixpkgs.follows = "rust-overlay/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = inputs: with inputs;
+  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        code = pkgs.callPackage ./. { inherit nixpkgs system rust-overlay; };
-      in rec {
-        packages = {
-          argo-helm-updater = code.argo-helm-updater;
-          default = packages.argo-helm-updater;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        craneLib = (crane.mkLib pkgs);
+
+        argo-helm-updater = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+          buildInputs = [
+            # Add additional build inputs here
+            pkgs.libiconv
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.darwin.apple_sdk.frameworks.Cocoa
+            pkgs.darwin.apple_sdk.frameworks.CoreGraphics
+            pkgs.darwin.apple_sdk.frameworks.Foundation
+            pkgs.darwin.apple_sdk.frameworks.IOKit
+            pkgs.darwin.apple_sdk.frameworks.Kernel
+            pkgs.darwin.apple_sdk.frameworks.OpenGL
+            pkgs.darwin.apple_sdk.frameworks.Security
+            pkgs.libpng
+            pkgs.zlib
+          ];
+        };
+      in {
+        checks = {
+          inherit argo-helm-updater;
+        };
+
+        packages.default = argo-helm-updater;
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+
+          packages = with pkgs; [
+            cargo-audit
+            cargo-edit
+            cargo-watch
+            clippy
+            libiconv
+          ];
         };
       }
     );
